b382cd29c431a3a1f4b4bbdb55c52ddf27114f7c
maheshchhagani <78026591+maheshchhagani@users.noreply.github.com>
2022-01-29 15:34:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php 
+    //include header.php
+    include('includes/header.php');
+
+?>
+    <!-- carousel start -->
+    <section id="banner-area">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="./assets/Banner1.png" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/Banner2.png" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/Banner1.png" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </section>
+    <!--carousel End -->
+
+    <!-- Promotion Banners Start -->
+    <div class="promobanner">
+      <div>
+        <a href="#">
+          <img src="./assets/b1.jpeg" alt="">
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img src="./assets/b2" alt="">
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img src="./assets/b3" alt="">
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img src="./assets/b4" alt="">
+        </a>
+      </div>
+      <div>
+        <a href="#">
+          <img src="./assets/b5" alt="">
+        </a>
+      </div>
+    </div>
+    <!-- Promotion Banners End -->
+
+    <!-- AK Promise Cards -->
+    <section>
+      <h3 style="text-align:center;">AK Jewelers Promise</h3>
+      <div class="card-columns d-flex">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="./assets/Banner1.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- AK Promise Cards -->
+
+    <!-- Testimonial Start -->
+    <h3 style="text-align:center;">Customer Says</h3>
+    <section id="testimonials">
+
+      <div id="testimonials-carousel" class="carousel slide" data-ride="false">
+        <div class="carousel-inner">
+          <div class="carousel-item car-itm-p active">
+            <h2>I no longer have to sniff other dogs for love. I've found the hottest Corgi on TinDog. Woof.</h2>
+            <img class="testimonial-img" src="images/dog-img.jpg" alt="dog-profile">
+            <em>Pebbles, New York</em>
+          </div>
+          <div class="carousel-item car-itm-p">
+            <h2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of
+              their life. I think.</h2>
+            <img class="testimonial-img" src="images/lady-img.jpg" alt="lady-profile">
+            <em>Beverly, Illinois</em>
+          </div>
+          <div class="carousel-item car-itm-p">
+            <h2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of
+              their life. I think.</h2>
+            <img class="testimonial-img" src="images/lady-img.jpg" alt="lady-profile">
+            <em>Beverly, Illinois</em>
+          </div>
+          <div class="carousel-item car-itm-p">
+            <h2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of
+              their life. I think.</h2>
+            <img class="testimonial-img" src="images/lady-img.jpg" alt="lady-profile">
+            <em>Beverly, Illinois</em>
+          </div>
+
+          <a href="#testimonials-carousel" class="carousel-control-prev" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a href="#testimonials-carousel" class="carousel-control-next" role="button" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
+        </div>
+      </div>
+
+
+    </section>
+    <!-- Testimonial End -->
+  </main>
+  <!-- Start main site -->
+
+  <?php 
+    //include header.php
+    include('includes/footer.php');
+
+?>
+
+  
